cae760a2c75d4a8f762fa4d03e42a5e50858380e
AKSpan <alex-kot-spb@mail.ru>
2015-12-23 11:09:22 +0300

    Auth support

--- props.js
+++ props.js
@@ -1,4 +1,6 @@
-var token ='';
+var token = '<TOKEN>';
 module.exports.token = token;
-var url ='mongodb://localhost:27017/akdb';
+var DB_USERNAME = '<USER>',
+    DB_PASSWORD = '<PASS>';
+var url = 'mongodb://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + 'localhost:27017/akdb';
 module.exports.url = url;
